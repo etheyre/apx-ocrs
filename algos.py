8d3335e52566081711e585e84ea25fc3427ab857
emilent <emile.touileb@ens-lyon.fr>
2023-06-14 16:01:06 +0200

    hop

--- algos.py
+++ algos.py
@@ -116,6 +116,7 @@ def mu_match(i, matching, rounded_weights, Q, y, fairness, demands, viewers_left
 					tot_demand += 1
 				
 #				print("rec match", lightest_viewer, len(Q[lightest_viewer]))
+				print("rematch ", lightest_viewer, len(matching[lightest_viewer]))
 				tot_demand = mu_match(lightest_viewer, matching, rounded_weights, Q, y,
 						              fairness, demands, viewers_left, tot_demand, b)
 				# TODO here we don't return?
